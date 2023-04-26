4362a4cab13655988df8e0a24af92bd5f00a8744
blackpirat01 <patrick.pira@t-online.de>
2023-04-26 14:27:49 +0200

    Aufgabe 2 Ordner + Beispiele hinzugefügt

--- Aufgabe1/simple-fork.c
+++ Aufgabe1/simple-fork.c
@@ -5,6 +5,10 @@ int simpleFork(int arcg, char *argv[]) {
     fork();
     fork();
     fork();//2x4 Hello = 8
+    fork();
+    fork();
+
+
 
     printf("Hello!\n");
     sleep(1);
--- main.cpp
+++ main.cpp
@@ -5,9 +5,12 @@
 #include "Aufgabe1/fork-example.c"
 #include "Aufgabe1/nChildprocess.c"
 #include "Aufgabe1/forknr2c.c"
+#include "Aufgabe2/example/fileReader.c"
 
 int main() {
-    nChildProcess(5);
+    //nChildProcess(3);
+    //simpleFork(0,0);
+    fileReader();
 
    return 0;
 }
