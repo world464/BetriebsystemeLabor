edf3c8875834c4f1cff92801d502edb020ff7318
blackpirat01 <patrick.pira@t-online.de>
2023-05-22 16:35:52 +0200

    Aufgabe 3 fertig

--- Aufgabe3/fifo_recv.c
+++ Aufgabe3/fifo_recv.c
@@ -1,3 +1,14 @@
-int receive() {
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#define BUFSIZE 1024
+
+int receive(int argc, char* argv[]) {
+    int fd = open("my_pipe",O_RDONLY);
+    char input_buffer[BUFSIZE];
+    for(int i = 100; i >= 0; i--) {
+        read(fd, input_buffer, sizeof(BUFSIZE));
+        printf("%s \n", input_buffer);
+    }
     return 0;
 }
--- Aufgabe3/fifo_send.c
+++ Aufgabe3/fifo_send.c
@@ -1,3 +1,22 @@
-int send() {
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[]) {
+    char output[3];
+    mkfifo("my_pipe",0777);
+    int fd = open("my_pipe",O_WRONLY);
+    for(int i = 100; i >= 0; i--){
+        sleep(1);
+        sprintf(output, "%d", i);
+        write(fd, output, sizeof(output));
+    }
+
+
     return 0;
 }
