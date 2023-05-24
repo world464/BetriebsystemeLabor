79e6baa49db3482937bed364419b9c0f0f55f40f
world464 <world.unger@web.de>
2023-05-24 11:16:12 +0200

    3c funktioniert

--- Aufgabe3/fifo_recv.c
+++ Aufgabe3/fifo_recv.c
@@ -1,11 +1,32 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
-#define BUFSIZE 1024
 
-int receive(int argc, char* argv[]) {
+#include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#define BUFSIZE 1024 * 10
+
+
+
+
+int main(int argc, char* argv[]) {
+    mkfifo("my_pipe", 0777);
+
     int fd = open("my_pipe",O_RDONLY);
     char input_buffer[BUFSIZE];
+
+    //Wait until receives data
+    while(read(fd, input_buffer, sizeof(BUFSIZE)) <= 0){
+        printf("%s \n", "Wartet!");
+    }
+
+    // Reset the file pointer to the start of the pixel data
+    lseek(fd, 0, SEEK_SET);
+
+    //Prints out data
     for(int i = 100; i >= 0; i--) {
         read(fd, input_buffer, sizeof(BUFSIZE));
         printf("%s \n", input_buffer);
--- Aufgabe3/fifo_send.c
+++ Aufgabe3/fifo_send.c
@@ -7,16 +7,18 @@
 #include <sys/types.h>
 #include <fcntl.h>
 
+//gcc fifo_recv.c -o fifo_recv && gcc fifo_send.c -o fifo_send
+//./fifo_recv man muss es erst kompelieren!!!
+
 int main(int argc, char* argv[]) {
     char output[3];
-    mkfifo("my_pipe",0777);
+
     int fd = open("my_pipe",O_WRONLY);
     for(int i = 100; i >= 0; i--){
         sleep(1);
         sprintf(output, "%d", i);
         write(fd, output, sizeof(output));
     }
-
 
     return 0;
 }
